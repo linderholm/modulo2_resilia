a8a6dcaa3fff25ff132f93b8fccb354f966d1c69
NodjaLima <nodjamplima@gmail.com>
2022-05-24 17:40:40 -0300

    repara bugs de cadastro e adiciona modal de verificação de dados

--- src/front/controller/controller_cadastro_cep.js
+++ src/front/controller/controller_cadastro_cep.js
@@ -1,43 +1,86 @@
-// function TestaCPF(strCPF) {
-//     var Soma;
-//     var Resto;
-//     Soma = 0;
-//   if (strCPF == "00000000000") return false;
-
-//   for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-//   Resto = (Soma * 10) % 11;
-
-//     if ((Resto == 10) || (Resto == 11))  Resto = 0;
-//     if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
-
-//   Soma = 0;
-//     for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
-//     Resto = (Soma * 10) % 11;
-
-//     if ((Resto == 10) || (Resto == 11))  Resto = 0;
-//     if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
-//     return true;
-// }
-
-// function validacaoEmail(email) {
-//   usuario = email.value.substring(0, email.value.indexOf("@"));
-//   dominio = email.value.substring(email.value.indexOf("@")+ 1, email.value.length);
+$('#cadastro-cep').on('blur', function() {
+  let cep = $("#cadastro-cep").val()
   
-//   if ((usuario.length >=1) &&
-//       (dominio.length >=3) &&
-//       (usuario.search("@")==-1) &&
-//       (dominio.search("@")==-1) &&
-//       (usuario.search(" ")==-1) &&
-//       (dominio.search(" ")==-1) &&
-//       (dominio.search(".")!=-1) &&
-//       (dominio.indexOf(".") >=1)&&
-//       (dominio.lastIndexOf(".") < dominio.length - 1)) {
-//       return true;
-//   }
-//   else{
-//     return false;
-//   }
-//   }
+  $.ajax({
+    url: `https://viacep.com.br/ws/${cep}/json/`,
+    type: "GET",
+    dataType: 'json',
+
+    success: function(result) {
+      console.log(result);
+
+      if (result.erro == "true") {
+        $('#cadastro-cep').removeClass("form-control").addClass("form-control is-invalid");
+      } else {
+        $('#cadastro-cidade').val(result.localidade);
+        $('#cadastro-estado').val(result.uf);
+        $('#cadastro-bairro').val(result.bairro);
+        $('#cadastro-rua').val(result.logradouro);
+      }
+          
+    },
+
+    error: function() {
+      $('#cadastro-cep').removeClass("form-control").addClass("form-control is-invalid");
+    }
+
+  })
+  
+})
+
+function validaCPF(strCPF) {
+  var Soma;
+  var Resto;
+  Soma = 0;
+if (strCPF == "00000000000") return false;
+
+for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+Resto = (Soma * 10) % 11;
+
+  if ((Resto == 10) || (Resto == 11))  Resto = 0;
+  if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+Soma = 0;
+  for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+  Resto = (Soma * 10) % 11;
+
+  if ((Resto == 10) || (Resto == 11))  Resto = 0;
+  if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+  return true;
+}
+
+
+
+$('#cadastro-cpf').on('blur', () => {
+ let cpf = $('#cadastro-cpf').val()
+  if (validaCPF(cpf) === false) {
+    $('#cadastro-cpf').removeClass("form-control").addClass("form-control is-invalid");
+    alert("verifique o cpf")
+  } else {
+    $('#cadastro-cpf').removeClass("form-control is-invalid").addClass("form-control");
+  }
+}) 
+
+function validacaoEmail(email) {
+  if (email.indexOf('@') == -1 || email.indexOf('.') == -1 || email.indexOf(" ") > 0 || email.length < 10) {
+  return false;
+} else {
+  return true;
+}
+}
+
+$('#cadastro-email').on('blur', () => {
+  if (validacaoEmail($('#cadastro-email').val()) == false) {
+    $('#cadastro-email').removeClass("form-control").addClass("form-control is-invalid");
+  }else {
+    $('#cadastro-email').removeClass("form-control is-invalid").addClass("form-control");
+  }
+})
+
+
+
+let cliente = []
+let endereco = []
 
 class Cadastro {
   constructor () {
@@ -45,6 +88,7 @@ class Cadastro {
   }
 
   addCadastro () {
+
     const nome = $("#cadastro-nome").val();
     const sobrenome = $("#cadastro-sobrenome").val();
     const rg = $("#cadastro-rg").val();
@@ -59,9 +103,9 @@ class Cadastro {
     const numero = $("#cadastro-numero").val();
     const complemento = $("#cadastro-complemento").val();
 
-    const cliente = new Cliente(nome, sobrenome, rg, cpf, email, senha);
+    cliente = new Cliente(nome, sobrenome, rg, cpf, email, senha);
 
-    const endereco = new Endereco(cep, rua, cidade, estado, bairro, complemento, numero);
+    endereco = new Endereco(cep, rua, cidade, estado, bairro, complemento, numero);
 
     this.cadastro.push(cliente);
     this.cadastro.push(endereco);
@@ -71,12 +115,41 @@ class Cadastro {
 
 }
 
-let cadastroFinal = new Cadastro()
-$("#btn-submit").on('click', function(event){
-  event.preventDefault();
-  console.log(cadastroFinal.addCadastro())
+function valida () {
+  'use strict';
+  window.addEventListener('load', function() {
+ 
+    var forms = document.getElementsByClassName('needs-validation');
 
-  // let formulario = document.querySelector("#formulario-cadastro");
-  // formulario.reset();
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('click', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+          
+        }
+        else {
+          form.classList.add('was-validated');
+        }
+        
+      }, false);
+    });
+  }, false);
+};
 
+valida()
+
+$("#btn-submit").on("click", function (evento){
+  evento.preventDefault()
+  
+  let cadastro = new Cadastro
+  cadastro = cadastro.addCadastro()
+  
+  mostraModal('ok')
+ 
 })
+
+let modal = document.querySelector("#exampleModal");
+  modal.addEventListener('click', function (){
+    $('#exampleModal').addClass('d-none')
+  })
